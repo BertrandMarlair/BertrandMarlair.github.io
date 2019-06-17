085d3f482ccfbe8a8d9efa982bcafa416c171dbc
Bertrand <bertrand@becode.org>
2019-06-17 23:46:50 +0200

    Updates

---
+++ precache-manifest.ce1462ab9c3ef58b37c9940c401eb757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae23487340559543aa7fe956acff070d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fce504950695dfc374f",
+    "url": "/static/css/2.115c5b3f.chunk.css"
+  },
+  {
+    "revision": "f757b540f4761cebadbc",
+    "url": "/static/css/main.cb34a77a.chunk.css"
+  },
+  {
+    "revision": "6fce504950695dfc374f",
+    "url": "/static/js/2.943ac108.chunk.js"
+  },
+  {
+    "revision": "f757b540f4761cebadbc",
+    "url": "/static/js/main.d83aba78.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
